c650a42b6792ec0c3fe1e33ba1e4ab7eef1da929
David Petek Pavković <dpetekpav@adnet.local>
2020-09-25 15:40:07 +0200

    Error handling

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,16 @@ import { IActivity } from "../models/activity";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
+axios.interceptors.response.use(undefined, error=>{
+  console.log(error.response);
+})
+
+axios.interceptors.response.use(undefined, error=>{
+  if(error.response.status === 404){
+    throw error.response;
+  }
+})
+
 const responseBody = (response: AxiosResponse) => response.data;
 
 const sleep = (ms: number) => (response: AxiosResponse) =>
