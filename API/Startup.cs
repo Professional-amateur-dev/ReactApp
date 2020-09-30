5b7b1f0d6e8cf1538afc2f5286372c7ecaae653b
David Petek Pavković <dpetekpav@adnet.hr>
2020-09-30 16:31:40 +0200

    User Accessor

--- API/Startup.cs
+++ API/Startup.cs
@@ -72,6 +72,7 @@ namespace API
                 });
 
             services.AddScoped<IJwtGenerator, JWtGenerator>();
+            services.AddScoped<IUserAccessor, UserAccessor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
